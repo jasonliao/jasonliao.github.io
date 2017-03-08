3ca08c9994604e7abe61ea47c5ca41464a7a1faf
jasonliao <lmovingon2014@gmail.com>
2017-03-08 11:50:50 +0800

    feature(about): jump to pdf if user use mobile

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,18 @@ var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
-  gulp.src('assets/stylesheets/*.css')
+  gulp.src(['assets/stylesheets/*.css', '!assets/stylesheets/about.cssa'])
     .pipe(minifyCSS())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('assets/stylesheets'))
 });
+
+gulp.task('about', function() {
+  gulp.src('assets/stylesheets/about.css')
+    .pipe(minifyCSS())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(concat('about.min.css'))
+    .pipe(gulp.dest('assets/stylesheets'))
+
+})
